ecb9a58182f90785b92aa7c05d1d5393f480d283
rhiriyappa <rhiriyappa@yahoo.com>
2021-04-06 14:00:39 -0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
+# from sklearn.externals import joblib  -- not supported anymore, import joblib as below
+import joblib
 from sklearn.externals import joblib
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
